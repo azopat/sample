4f1b68049e334dda04021e349b540a0862a605b7
Jenkins Continuous Build server <jenkins@wahala.akoden.com>
2011-10-07 03:30:16 +0100

    sa

--- sample/settings.py
+++ sample/settings.py
@@ -9,12 +9,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sample',                      # Or path to database file if using sqlite3.
+        'USER': 'sample',                      # Not used with sqlite3.
+        'PASSWORD': 'welcome1',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -109,8 +110,8 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'hello'
-    'django_jenkins'
+    'hello',
+    'django_jenkins',
     #'django.contrib.auth',
     #'django.contrib.contenttypes',
     #'django.contrib.sessions',
